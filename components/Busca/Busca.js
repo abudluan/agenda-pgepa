3d517ba99d53808529aa3ae736078269f8ca4206
abudluan <abudluan@gmail.com>
2023-08-03 13:01:25 -0300

    feat: component search added

--- components/Busca/Busca.js
+++ components/Busca/Busca.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import ClearIcon from 'react-native-vector-icons/MaterialIcons';
 
 import {
+    Container,
+    SearchInput,
 
 } from './BuscaStyles';
 
 const Busca = () => {
     const [searchValue, setSearchValue] = useState('');
-    
+
     return (
-        <>
-        </>
+        <Container>
+            <SearchInput
+                placeholder="Digite sua pesquisa..."
+                placeholderTextColor="#fff"
+                selectionColor="white"
+            />
+
+            <ScrollView>
+
+            </ScrollView>
+
+        </Container>
     )
 };
 
---
+++ components/Busca/BuscaStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    height: 100%;
+    background-color: #FFFFFF;
+`;
+
+export const SearchInput = styled.TextInput`
+    margin: auto;
+    margin-top: 18px;
+    width: 95%;
+    height: 40px;
+    padding-left: 10px;
+    border-radius:5px;
+    background-color: #008000;
+    color: white;
+`;
